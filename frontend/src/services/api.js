f1231da3ee5705820003c414c7460136b3d9b0cc
sapkotas <sunavsapkota57@gmail.com>
2026-07-30 19:56:18 +0545

    Updated api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api/v1',
+  baseURL: 'https://multi-user-project-management-jkd4.vercel.app/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
